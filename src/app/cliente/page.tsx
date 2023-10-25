12ed178e31b4f69ca3b8b6b989ac33843532e98d
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-25 15:10:19 -0300

    Tratamento de erro

--- src/app/cliente/page.tsx
+++ src/app/cliente/page.tsx
@@ -5,35 +5,40 @@ import ExibeCep from "./exibe-cep";
 
 export default function CepPaginaCliente() {
   const [dados, setDados] = useState({})
+  const [erro, setErro] = useState('')
 
   const submitForm = async (event) => {
     event.preventDefault()
     const cep = event.target.cep.value
 
-    const res = await fetch(
-      `https://viacep.com.br/ws/${cep}/json/`, 
-      {
-        headers: { 'Access-Control-Allow-Origin': '*' }
-      }
-    )
+    try {
+      const res = await fetch(
+        `https://viacep.com.br/ws/${cep}/json/`, 
+        {
+          headers: { 'Access-Control-Allow-Origin': '*' }
+        }
+      )
 
-    if (!res.ok) {
-      // TODO elemento de erro
-      return console.error('Erro!')
+      setErro('')
+      setDados(await res.json())
+    } catch (e) {
+      setErro(e+'')
+      setDados({})
     }
-
-    setDados(await res.json())
-    
   }
 
   return (
     <>
       <h1>Consulta no ViaCEP</h1>
+
+      {erro && <p>Erro: {erro}</p>}
+
       <form onSubmit={submitForm}>
         <label htmlFor="cep">CEP:</label>
         <input id="cep" name="cep" type="text" />
         <button type="submit">Consultar</button>
       </form>
+
       <ExibeCep dadosViaCep={dados} />
     </>
   )
