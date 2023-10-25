6a074a7eb14e2b939d9e404cffeeddaf053a5ccf
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-25 14:18:10 -0300

    Buscando dados do servidor

--- src/app/servidor/[cep]/page.tsx
+++ src/app/servidor/[cep]/page.tsx
@@ -1,3 +1,20 @@
-export default function CepPaginaServidor({ params }) {
-  return <p>CEP a consultar: {params.cep}</p>
+export default async function CepPaginaServidor({ params }) {
+  const res = await fetch(`https://viacep.com.br/ws/${params.cep}/json/`)
+
+  if (!res.ok) {
+    return <p>Erro!</p>
+  }
+
+  const dados = await res.json()
+
+  return (
+    <>
+      <h1>{dados.cep}</h1>
+      {Object.keys(dados).map((k, i) => 
+        <p key={i}>
+          <b>{k}:</b> {dados[k]}
+        </p>  
+      )}
+    </>
+  )
 }
