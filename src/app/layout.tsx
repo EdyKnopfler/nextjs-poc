b4150139aade2b1ea9c29a6ce4ffda94f49c896c
Éderson Cássio <edersoncassio1983@gmail.com>
2023-10-25 15:25:15 -0300

    Home e navegação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <nav className='navegacao-principal'>
+          <Link href="/">Início</Link>
+          <Link href="/servidor/37550001">Página renderizada no servidor</Link>
+          <Link href="/cliente">Página renderizada no cliente</Link>
+        </nav>
         <main className="main">
           {children}
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <></>
+    <h1 className={styles.titulo}>Bem-vindo!</h1>
   )
 }
